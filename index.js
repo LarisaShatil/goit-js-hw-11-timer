ead2e97eb202621bbef546a0c72482e07c90a2e0
LarisaShatil <larisa.shat@ukr.net>
2021-08-14 21:42:41 +0300

    14.08

---
+++ index.js
@@ -0,0 +1,84 @@
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
+
+class CountdownTimer {
+  constructor() {
+  this.refs = {
+  days: document.querySelector("[data-value='days']"),
+  hours: document.querySelector("[data-value='hours']"),
+  mins: document.querySelector("[data-value='mins']"),
+  secs: document.querySelector("[data-value='secs']"),
+    };
+    this.id = null;
+    this.currentDate = null;
+
+    this.startTimer = this.startTimer.bind(this);
+    
+  document.addEventListener('DOMContentLoaded', this.startTimer);
+  };
+  
+calc() {
+  this.currentDate = Date.now();
+  const targetDate = new Date(2021, 9, 1);
+  const time = targetDate - this.currentDate;
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+ 
+  this.refs.days.textContent = days < 10 ? `0${days}` : days;
+  this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+  this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
+  this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
+  };
+
+  startTimer() {
+    console.log(this);
+    this.currentDate = Date.now();
+    this.id = setInterval(this.calc, 1000);
+  };
+  
+}
+
+const holidayTimer = new CountdownTimer;
+
+
+
+
+
+
+// Plain WORKING code
+// const refs = {
+//   days: document.querySelector("[data-value='days']"),
+//   hours: document.querySelector("[data-value='hours']"),
+//   mins: document.querySelector("[data-value='mins']"),
+//   secs: document.querySelector("[data-value='secs']"),
+
+// };
+
+// function calc() {
+//   const currentDate = Date.now();
+//   const targetDate = new Date(2021, 08, 01);
+//   const delta = (targetDate - currentDate) / 1000;
+
+//   const days = Math.floor(delta / 60 / 60 / 24);
+//   const hours = Math.floor(delta / 60 / 60) % 24;
+//   const mins = Math.floor(delta / 60) % 60;
+//   const secs = Math.floor(delta) % 60;
+
+//   if (currentDate > targetDate) {
+//     return 
+//   } else {
+//   refs.days.textContent = days < 10 ? `0${days}` : days;
+//   refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+//   refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
+//   refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
+//   }
+
+// };
+
+// console.log(calc);
+// document.addEventListener('DOMContentLoaded', setInterval(calc, 1000));
+
